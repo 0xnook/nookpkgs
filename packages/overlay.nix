7ed25e891f528b53d4d12d87d0d8fda09faaef11
nook <0xnook@protonmail.com>
2022-05-28 16:00:31 +0200

    fix: add missing file extension

--- packages/overlay.nix
+++ packages/overlay.nix
@@ -1,4 +1,4 @@
 final: prev: {
 	nookpkgs.frame = prev.pkgs.callPackage ./frame.nix {};
-	nookpkgs.vimPlugins.vim-svelte-plugin = prev.pkgs.callPackage ./vimplugins/vim-svelte-plugin {};
+	nookpkgs.vimPlugins.vim-svelte-plugin = prev.pkgs.callPackage ./vimplugins/vim-svelte-plugin.nix {};
 }
